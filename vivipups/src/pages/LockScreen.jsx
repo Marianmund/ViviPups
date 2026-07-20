1a180117c49264b128297255a99efb6d01c49f93
Marian <marian@fit-for-fire.eu>
2026-07-20 20:49:25 +0200

    Test

--- vivipups/src/pages/LockScreen.jsx
+++ vivipups/src/pages/LockScreen.jsx
@@ -1,9 +1,37 @@
+const numbers = [
+    { num: 1 },
+    { num: 2 },
+    { num: 3 },
+    { num: 4 },
+    { num: 5 },
+    { num: 6 },
+    { num: 7 },
+    { num: 8 },
+    { num: 9 },
+    { num: 0 }
+];
+
+function Buttons() {
+    return (
+        <div>
+            {numbers.map((number) => (
+                <div key={number.num}>
+                    <h1>{number.num}</h1>
+                </div>
+            ))}
+        </div>
+    );
+}
+
 function LockScreen() {
     return (
         <main className="min-h-screen flex items-center justify-center">
-            <h1 className="text-5xl font-bold text-pink-600">
-                LockScreen
-            </h1>
+            <div>
+                <h1 className="text-5xl font-bold text-pink-600">
+                    LockScreen
+                </h1>
+                <Buttons />
+            </div>
         </main>
     );
 }
